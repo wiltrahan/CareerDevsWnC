1dd9ad572547c8ae44c200e54db6830fe4e5f17f
wiltrahan <twilorip@gmail.com>
2017-07-08 17:19:10 -0400

    complete lesson 3

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ todos[0] = "changed 1";
 //It should have a way to delete a todo
 todos.splice(0, 1);
 //todos is now ['item 2', 'item 3', 'item 4'];
+//////////////END LESSON//////////////
 
 //lesson 2 ***FUNCTIONS***
 
@@ -29,7 +30,7 @@ function addTodo(todo) {
   todos.push(todo);
   displayTodos();
 }
-addTodo('kick butt');
+addTodo("kick butt");
 
 //It should have a function to change todos
 function changeTodo(position, newValue) {
@@ -45,3 +46,66 @@ function deleteTodo(position) {
   displayTodos();
 }
 deleteTodo(0);
+//////////////END LESSON//////////////
+
+//lesson 3 ***OBJECTS***
+
+//It should store the todos array on an object
+var todoList = {
+  todos: ["item 1", "item 2", "item 3"]
+};
+
+//It should have a display todos method
+var todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function() {
+    console.log("My Todos:", this.todos);
+  }
+};
+
+//It should have an addTodo method
+var todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function() {
+    console.log("My Todos:", this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  }
+};
+//It should have a changeTodo method
+var todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function() {
+    console.log("My Todos:", this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  }
+};
+//It should have a deleteTodo method
+var todoList = {
+  todos: ["item 1", "item 2", "item 3"],
+  displayTodos: function() {
+    console.log("My Todos:", this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
+//////////////END LESSON//////////////
