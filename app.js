53846c7f8c7926aef47b42c1a1acacef7fee9074
wiltrahan <twilorip@gmail.com>
2017-07-08 19:15:22 -0400

    complete lesson 5

--- app.js
+++ app.js
@@ -160,11 +160,125 @@
 // todoList.addTodo('first try');
 // todoList.changeTodo(0, 'second try');
 
-//todoList.toggleCompleted should change the completed property
+//***todoList.toggleCompleted should change the completed property***
+// var todoList = {
+//   todos: [],
+//   displayTodos: function() {
+//     console.log("My Todos:", this.todos);
+//   },
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//     this.displayTodos();
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//     this.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//     this.displayTodos();
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//     this.displayTodos();
+//   }
+// };
+// todoList.addTodo("finish this one.");
+// todoList.toggleCompleted(0);
+//////////////END LESSON//////////////
+
+//lesson 5 ***LOOPS***
+
+//***.displayTodos should show .todoText***
+// var todoList = {
+//   todos: [],
+//   displayTodos: function() {
+//     console.log("My Todos:");
+//     for(var i = 0; i < this.todos.length; i++) {
+//       console.log(this.todos[i].todoText);
+//     }
+//   },
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//     this.displayTodos();
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//     this.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//     this.displayTodos();
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//     this.displayTodos();
+//   }
+// };
+
+// todoList.addTodo("first");
+
+//***.displayTodos should tell you if .todos is empty***
+// var todoList = {
+//   todos: [],
+//   displayTodos: function() {
+//     if (this.todos.length === 0) {
+//       console.log("Your todo list is empty!");
+//     } else {
+//       console.log("My Todos:");
+//       for (var i = 0; i < this.todos.length; i++) {
+//         console.log(this.todos[i].todoText);
+//       }
+//     }
+//   },
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//     this.displayTodos();
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//     this.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//     this.displayTodos();
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//     this.displayTodos();
+//   }
+// };
+// // todoList.addTodo("kick ass!");
+// todoList.displayTodos();
+
+//***.displayTodos should show .completed***
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos:", this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log("(x)", this.todos[i].todoText);
+        } else {
+          console.log("( )", this.todos[i].todoText);
+        }
+      }
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -187,6 +301,7 @@ var todoList = {
     this.displayTodos();
   }
 };
-todoList.addTodo("finish this one.");
-todoList.toggleCompleted(0);
+todoList.addTodo("kick ass!");
+todoList.addTodo("kick more ass!");
+todoList.toggleCompleted(1);
 //////////////END LESSON//////////////
