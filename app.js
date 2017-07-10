3aee39c9b55a0d2cdc69f6ddd71d434f1f3be948
wiltrahan <twilorip@gmail.com>
2017-07-10 11:44:01 -0400

    complete lesson 6

--- app.js
+++ app.js
@@ -264,6 +264,109 @@
 // todoList.displayTodos();
 
 //***.displayTodos should show .completed***
+// var todoList = {
+//   todos: [],
+//   displayTodos: function() {
+//     if (this.todos.length === 0) {
+//       console.log("Your todo list is empty!");
+//     } else {
+//       console.log("My Todos:");
+//       for (var i = 0; i < this.todos.length; i++) {
+//         if (this.todos[i].completed === true) {
+//           console.log("(x)", this.todos[i].todoText);
+//         } else {
+//           console.log("( )", this.todos[i].todoText);
+//         }
+//       }
+//     }
+//   },
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//     this.displayTodos();
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//     this.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//     this.displayTodos();
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//     this.displayTodos();
+//   }
+// };
+// todoList.addTodo("kick ass!");
+// todoList.addTodo("kick more ass!");
+// todoList.toggleCompleted(1);
+//////////////END LESSON//////////////
+
+//lesson 6 ***THINKING IN CODE***
+
+//***.toggleAll: if everything's true, make everything false.***
+// var todoList = {
+//   todos: [],
+//   displayTodos: function() {
+//     if (this.todos.length === 0) {
+//       console.log("Your todo list is empty!");
+//     } else {
+//       console.log("My Todos:");
+//       for (var i = 0; i < this.todos.length; i++) {
+//         if (this.todos[i].completed === true) {
+//           console.log("(x)", this.todos[i].todoText);
+//         } else {
+//           console.log("( )", this.todos[i].todoText);
+//         }
+//       }
+//     }
+//   },
+//   addTodo: function(todoText) {
+//     this.todos.push({
+//       todoText: todoText,
+//       completed: false
+//     });
+//     this.displayTodos();
+//   },
+//   changeTodo: function(position, todoText) {
+//     this.todos[position].todoText = todoText;
+//     this.displayTodos();
+//   },
+//   deleteTodo: function(position) {
+//     this.todos.splice(position, 1);
+//     this.displayTodos();
+//   },
+//   toggleCompleted: function(position) {
+//     var todo = this.todos[position];
+//     todo.completed = !todo.completed;
+//     this.displayTodos();
+//   },
+//   toggleAll: function() {
+//     var totalTodos = this.todos.length;
+//     var completedTodos = 0;
+
+//     for (var i = 0; i < totalTodos; i++) {
+//       if (this.todos[i].completed === true) {
+//         completedTodos++;
+//       }
+//     }
+//     if (completedTodos === totalTodos) {
+//       for (var i = 0; i < totalTodos; i++) {
+//         this.todos[i].completed = false;
+//       }
+//     }
+//     this.displayTodos();
+//   }
+// };
+// todoList.addTodo('first');
+// todoList.addTodo('second');
+// todoList.toggleAll();
+
+//***toggleAll: Otherwise, make everything true.***
 var todoList = {
   todos: [],
   displayTodos: function() {
@@ -299,9 +402,26 @@ var todoList = {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
   }
 };
-todoList.addTodo("kick ass!");
-todoList.addTodo("kick more ass!");
-todoList.toggleCompleted(1);
 //////////////END LESSON//////////////
